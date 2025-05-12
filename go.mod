b150fc8b2bdb8e5af249d4fe33b9b8962b795f0c
Andy Kreling <krelinga@gmail.com>
2025-05-12 14:16:26 +0000

    hello world server.

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/krelinga/video-in-be
 
 go 1.23.8
+
+require (
+	buf.build/gen/go/krelinga/proto/connectrpc/go v1.18.1-20250512135634-96ef563906bf.1
+	buf.build/gen/go/krelinga/proto/protocolbuffers/go v1.36.6-20250512135634-96ef563906bf.1
+	golang.org/x/net v0.23.0
+)
+
+require (
+	connectrpc.com/connect v1.18.1 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	pbconnect "buf.build/gen/go/krelinga/proto/connectrpc/go/krelinga/video/in/v1/inv1connect"
+	pb "buf.build/gen/go/krelinga/proto/protocolbuffers/go/krelinga/video/in/v1"
+	"connectrpc.com/connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+type ConnectService struct {
+}
+
+func (s *ConnectService) HelloWorld(ctx context.Context, req *connect.Request[pb.HelloWorldRequest]) (*connect.Response[pb.HelloWorldResponse], error) {
+	// Create a new HelloWorldResponse
+	response := &pb.HelloWorldResponse{
+		Message: "Hello, " + req.Msg.Name,
+	}
+
+	// Return the response
+	return connect.NewResponse(response), nil
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	path, handler := pbconnect.NewServiceHandler(&ConnectService{})
+	mux.Handle(path, handler)
+	// Runs as long as the server is alive.
+	http.ListenAndServe("0.0.0.0:25004", h2c.NewHandler(mux, &http2.Server{}))
 }
