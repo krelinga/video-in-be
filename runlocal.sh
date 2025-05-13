1b6f006be759ab299e26e66c0e3f719e52336e77
Andy Kreling <krelinga@gmail.com>
2025-05-13 17:51:33 +0000

    mount /nas/media in runlocal.sh

--- runlocal.sh
+++ runlocal.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker build -t video-in-be .
-docker run --rm -it -p 25004:25004 --name video-in-be-container video-in-be
+docker run --rm -v /nas/media:/nas/media -it -p 25004:25004 --name video-in-be video-in-be
