38bbbe159e062476c6533750a127c65d394e12f6
Andy Kreling <krelinga@gmail.com>
2025-05-14 18:40:00 +0000

    only print the first result, print other fields.

--- demo/search_movies.go
+++ demo/search_movies.go
@@ -19,8 +19,13 @@ func searchMovies() error {
 		fmt.Println("No movies found")
 		return nil
 	}
-	for _, movie := range found {
+	for i, movie := range found {
+		if i > 0 {
+			break
+		}
 		fmt.Printf("Found movie: %s (%s)\n", movie.Title, movie.RealaseDate.Format(time.DateOnly))
+		fmt.Printf("Original title: %s\n", movie.OriginalTitle)
+		fmt.Printf("ID: %d\n", movie.ID)
 		fmt.Printf("Genres: %s\n", strings.Join(movie.Genres, ", "))
 		fmt.Printf("Poster URL: %s\n", movie.PosterUrl)
 		fmt.Printf("Overview: %s\n", movie.Overview)
