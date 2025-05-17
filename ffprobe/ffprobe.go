d34ac20b1f6bf3d1674e7a79c60eae80089b7e82
Andy Kreling <krelinga@gmail.com>
2025-05-17 05:45:28 +0000

    helper methods to filter streams.

--- ffprobe/ffprobe.go
+++ ffprobe/ffprobe.go
@@ -2,6 +2,7 @@ package ffprobe
 
 import (
 	"fmt"
+	"iter"
 	"time"
 )
 
@@ -32,4 +33,31 @@ func (f *FFProbe) GetDuration() (time.Duration, bool) {
 		panic(fmt.Sprintf("error parsing duration %s : %v", f.Raw.Format.Duration, err))
 	}
 	return d, true
+}
+
+func (f *FFProbe) getStreams(streamType string) iter.Seq[*RawStream] {
+	return func(yield func(*RawStream) bool) {
+		if f.Raw == nil || f.Raw.Streams == nil {
+			return
+		}
+		for _, stream := range f.Raw.Streams {
+			if stream.CodecType == streamType {
+				if !yield(stream) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func (f *FFProbe) GetVideoStreams() iter.Seq[*RawStream] {
+	return f.getStreams("video")
+}
+
+func (f *FFProbe) GetAudioStreams() iter.Seq[*RawStream] {
+	return f.getStreams("audio")
+}
+
+func (f *FFProbe) GetSubtitleStreams() iter.Seq[*RawStream] {
+	return f.getStreams("dvd_subtitle")
 }
