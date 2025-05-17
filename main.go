1de19a1b17f89be98179f7dda87157ab834773ac
Andy Kreling <krelinga@gmail.com>
2025-05-17 04:51:33 +0000

    a flag for binary mode.

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/krelinga/video-in-be/server"
 )
 
+var mode string
+
+func init() {
+	flag.StringVar(&mode, "mode", "server", "Mode in which the application runs")
+}
+
 func main() {
-	server.Start()
+	flag.Parse()
+	fmt.Printf("Running in mode: %s\n", mode)
+
+	switch mode {
+	case "server":
+		server.Start()
+	case "demo":
+		// TODO: run demo
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown mode: %s, supported modes are 'server' and 'demo'\n", mode)
+		os.Exit(1)
+	}
 }
