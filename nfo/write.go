964e8b8cbafd349e082a1d1ac0f001deabb82d00
Andy Kreling <krelinga@gmail.com>
2025-05-17 06:39:55 +0000

    add XML headers.

--- nfo/write.go
+++ nfo/write.go
@@ -10,7 +10,12 @@ func (m *Movie) Write(writer io.Writer) error {
 	if err != nil {
 		return err
 	}
-	toWrite := [][]byte{x, []byte("\n")}
+	toWrite := [][]byte{
+		[]byte(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>` + "\n"),
+		[]byte("<!-- created by https://github.com/krelinga/video-in-be -->\n"),
+		x,
+		[]byte("\n"),
+	}
 	for _, b := range toWrite {
 		_, err = writer.Write(b)
 		if err != nil {
